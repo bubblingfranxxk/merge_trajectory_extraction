[fix]:trajectory extraction improved and TTC new calculation,final classify TTC class(MR/ML/AR/AL).

src/extraction/mergingExtractionClass.py
@@ -14,7 +14,6 @@ import shutil
 import matplotlib.pyplot as plt
 
 
-
 class MergingExtractionClass(object):
     def __init__(self, config):
         self.config = config
@@ -227,15 +226,26 @@ class MergingExtractionClass(object):
         self.vehicleType = self.Meta['class'].values[0]
 
         for vehicleType in self.surroundingVehiclesLabel:
+            # if vehicleType != "leftAlongsideId":
+            #     continue
+            # if ';' not in row[vehicleType]:
+            #     continue
             # print(vehicleType)
             # 周围车辆的属性
-            otherVehicleMeta = tracksMeta[tracksMeta['trackId'] == row[vehicleType]]
+            otherVehicleMeta = \
+                tracksMeta[tracksMeta['trackId'] ==
+                           int(row[vehicleType].split(';')[0]
+                               if vehicleType == "leftAlongsideId" and ';' in str(row[vehicleType])
+                               else row[vehicleType])]
             # otherLength = otherVehicleMeta['length'].values[0]
             # otherWidth = otherVehicleMeta['width'].values[0]
             # otherType = otherVehicleMeta['class'].values[0]
 
-            otherVehicleThisFrame = self.otherVehicle[(self.otherVehicle['trackId'] == row[vehicleType]) &
-                                                      (self.otherVehicle['frame'] == row['frame'])]
+            otherVehicleThisFrame = \
+                self.otherVehicle[(self.otherVehicle['trackId'] ==
+                                   int(row[vehicleType].split(';')[0]
+                                       if vehicleType == "leftAlongsideId" and ';' in str(row[vehicleType])
+                                       else row[vehicleType])) & (self.otherVehicle['frame'] == row['frame'])]
             if otherVehicleThisFrame.empty or row[vehicleType] == -1 or row[vehicleType] == "-999":
                 continue
             # print('xcenter:')
@@ -263,9 +273,10 @@ class MergingExtractionClass(object):
 
             if (row["recordingId"] in self.recordingMapToLocation["2"] or row["recordingId"] in self.
                     recordingMapToLocation["3"] or row["recordingId"] in self.recordingMapToLocation["5"]) \
-                    and positionLabel != "on -2":
+                    and (positionLabel != "on -2" and positionLabel != "on entry" and positionLabel != "on onramp"):
                 continue
-            elif row["recordingId"] in self.recordingMapToLocation["6"] and positionLabel != "on -3":
+            elif row["recordingId"] in self.recordingMapToLocation["6"] and \
+                    (positionLabel != "on -3" and positionLabel != "on entry" and positionLabel != "on onramp"):
                 continue
 
             status = "Exist"
@@ -273,13 +284,15 @@ class MergingExtractionClass(object):
             # 速度差和加速度差统一为汇入车-其他车
             deltaV = -(otherVehicleThisFrame["xVelocity"].values[0] * math.cos(theta) + otherVehicleThisFrame[
                 "yVelocity"].values[0] * math.sin(theta)) + row["xVelocity"] * math.cos(theta) + row["yVelocity"] * \
-                math.sin(theta)
+                     math.sin(theta)
             deltaAcce = -(otherVehicleThisFrame["xAcceleration"].values[0] * math.cos(theta) + otherVehicleThisFrame[
-                    "yAcceleration"].values[0] * math.sin(theta)) + row["xAcceleration"] * math.cos(theta) + row[
-                                         "yAcceleration"] * math.sin(theta)
+                "yAcceleration"].values[0] * math.sin(theta)) + row["xAcceleration"] * math.cos(theta) + row[
+                            "yAcceleration"] * math.sin(theta)
             thisFrame = self.laneChangeFrame["frame"].values[0]
             if vehicleType == "leadId":
                 self.leadVehicle = row[vehicleType]
+                # logger.warning(f"recording id {row['recordingId']}, track id {row['trackId']}. "
+                #                f"lead vehicle is {self.leadVehicle}")
                 self.leadDeltaX = distance
                 self.leadDeltaV = deltaV
                 self.leadDeltaAcce = deltaAcce
@@ -339,15 +352,15 @@ class MergingExtractionClass(object):
 
         surroundingInfo = {"vehicleNums": len(trajectoryInfo.keys()), "trajectory": trajectoryInfo}
         return surroundingInfo, self.rearVehicle, self.rearDeltaX, self.rearDeltaV, self.rearDeltaAcce, \
-            self.rearTTC_V1, self.rearTTC_V2, self.rearTTC_V3, \
-            self.leadVehicle, self.leadDeltaX, self.leadDeltaV, self.leadDeltaAcce, self.leadTTC_V1, self.leadTTC_V2, \
-            self.leadTTC_V3, \
-            self.leftrearVehicle, self.leftrearDeltaX, self.leftrearDeltaV, self.leftrearDeltaAcce, \
-            self.leftrearTTC_V1, self.leftrearTTC_V2, self.leftrearTTC_V3, \
-            self.leftleadVehicle, self.leftleadDeltaX, self.leftleadDeltaV, self.leftleadDeltaAcce, \
-            self.leftleadTTC_V1, self.leftleadTTC_V2, self.leftleadTTC_V3, \
-            self.leftalongsideVehicle, self.leftalongsideDeltaX, self.leftalongsideDeltaV, \
-            self.leftalongsideDeltaAcce, self.leftalongsideTTC_V1, self.leftalongsideTTC_V2, self.leftalongsideTTC_V3
+               self.rearTTC_V1, self.rearTTC_V2, self.rearTTC_V3, \
+               self.leadVehicle, self.leadDeltaX, self.leadDeltaV, self.leadDeltaAcce, self.leadTTC_V1, self.leadTTC_V2, \
+               self.leadTTC_V3, \
+               self.leftrearVehicle, self.leftrearDeltaX, self.leftrearDeltaV, self.leftrearDeltaAcce, \
+               self.leftrearTTC_V1, self.leftrearTTC_V2, self.leftrearTTC_V3, \
+               self.leftleadVehicle, self.leftleadDeltaX, self.leftleadDeltaV, self.leftleadDeltaAcce, \
+               self.leftleadTTC_V1, self.leftleadTTC_V2, self.leftleadTTC_V3, \
+               self.leftalongsideVehicle, self.leftalongsideDeltaX, self.leftalongsideDeltaV, \
+               self.leftalongsideDeltaAcce, self.leftalongsideTTC_V1, self.leftalongsideTTC_V2, self.leftalongsideTTC_V3
 
     def getTTC_V1(self):
         if self.leadVehicle and self.leadDeltaV > 0:
@@ -391,30 +404,30 @@ class MergingExtractionClass(object):
         blist = []
 
         BX1 = otherVehicleThisFrame['xCenter'].values[0] + 0.5 * length_other * math.cos(other_heading) \
-            + 0.5 * width_other * math.sin(other_heading)
+              + 0.5 * width_other * math.sin(other_heading)
         BY1 = otherVehicleThisFrame['yCenter'].values[0] + 0.5 * length_other * math.sin(other_heading) \
-            - 0.5 * width_other * math.cos(other_heading)
+              - 0.5 * width_other * math.cos(other_heading)
         B1 = {'x': BX1, 'y': BY1}
         blist.append(B1)
 
         BX2 = otherVehicleThisFrame['xCenter'].values[0] - 0.5 * length_other * math.cos(other_heading) \
-            + 0.5 * width_other * math.sin(other_heading)
+              + 0.5 * width_other * math.sin(other_heading)
         BY2 = otherVehicleThisFrame['yCenter'].values[0] - 0.5 * length_other * math.sin(other_heading) \
-            - 0.5 * width_other * math.cos(other_heading)
+              - 0.5 * width_other * math.cos(other_heading)
         B2 = {'x': BX2, 'y': BY2}
         blist.append(B2)
 
         BX3 = otherVehicleThisFrame['xCenter'].values[0] - 0.5 * length_other * math.cos(other_heading) \
-            - 0.5 * width_other * math.sin(other_heading)
+              - 0.5 * width_other * math.sin(other_heading)
         BY3 = otherVehicleThisFrame['yCenter'].values[0] - 0.5 * length_other * math.sin(other_heading) \
-            + 0.5 * width_other * math.cos(other_heading)
+              + 0.5 * width_other * math.cos(other_heading)
         B3 = {'x': BX3, 'y': BY3}
         blist.append(B3)
 
         BX4 = otherVehicleThisFrame['xCenter'].values[0] + 0.5 * length_other * math.cos(other_heading) \
-            - 0.5 * width_other * math.sin(other_heading)
+              - 0.5 * width_other * math.sin(other_heading)
         BY4 = otherVehicleThisFrame['yCenter'].values[0] + 0.5 * length_other * math.cos(other_heading) \
-            + 0.5 * width_other * math.cos(other_heading)
+              + 0.5 * width_other * math.cos(other_heading)
         B4 = {'x': BX4, 'y': BY4}
         blist.append(B4)
 
@@ -434,12 +447,12 @@ class MergingExtractionClass(object):
                 a1 = VA['y'] - VB['y']
                 b1 = VB['x'] - VA['x']
                 c1 = point['x'] * (VA['y'] - VB['y']) + point['y'] * (VB['x'] - VA['x'])
-                a2 = blist[i-1]['y'] - blist[i]['y']
-                b2 = blist[i]['x'] - blist[i-1]['x']
-                c2 = blist[i-1]['x'] * (blist[i-1]['y'] - blist[i]['y']) + blist[i-1]['y'] * (blist[i]['x'] -
-                                                                                              blist[i-1]['x'])
+                a2 = blist[i - 1]['y'] - blist[i]['y']
+                b2 = blist[i]['x'] - blist[i - 1]['x']
+                c2 = blist[i - 1]['x'] * (blist[i - 1]['y'] - blist[i]['y']) + blist[i - 1]['y'] * (blist[i]['x'] -
+                                                                                                    blist[i - 1]['x'])
                 A = np.array([[a1, b1],
-                             [a2, b2]])
+                              [a2, b2]])
                 B = np.array([c1, c2])
                 solution = np.linalg.solve(A, B)
                 min_x = min(b['x'] for b in blist[:-1])
@@ -448,7 +461,7 @@ class MergingExtractionClass(object):
                 max_y = max(b['y'] for b in blist[:-1])
                 # print(min_x, max_x, min_y, max_y)
                 if max_x >= solution[0] >= min_x and max_y >= solution[1] >= min_y:
-                    dist = math.sqrt((point['x'] - solution[0])**2 + (point['y'] - solution[1])**2)
+                    dist = math.sqrt((point['x'] - solution[0]) ** 2 + (point['y'] - solution[1]) ** 2)
                     dist_list.append(dist)
 
         for point in blist:
@@ -456,10 +469,10 @@ class MergingExtractionClass(object):
                 a1 = VA['y'] - VB['y']
                 b1 = VB['x'] - VA['x']
                 c1 = point['x'] * (VA['y'] - VB['y']) + point['y'] * (VB['x'] - VA['x'])
-                a2 = alist[i-1]['y'] - alist[i]['y']
-                b2 = alist[i]['x'] - alist[i-1]['x']
-                c2 = alist[i-1]['x'] * (alist[i-1]['y'] - alist[i]['y']) + alist[i-1]['y'] * (alist[i]['x'] -
-                                                                                              alist[i-1]['x'])
+                a2 = alist[i - 1]['y'] - alist[i]['y']
+                b2 = alist[i]['x'] - alist[i - 1]['x']
+                c2 = alist[i - 1]['x'] * (alist[i - 1]['y'] - alist[i]['y']) + alist[i - 1]['y'] * (alist[i]['x'] -
+                                                                                                    alist[i - 1]['x'])
                 A = np.array([[a1, b1],
                               [a2, b2]])
                 B = np.array([c1, c2])
@@ -472,7 +485,7 @@ class MergingExtractionClass(object):
                     dist = math.sqrt((point['x'] - solution[0]) ** 2 + (point['y'] - solution[1]) ** 2)
                     dist_list.append(dist)
         if dist_list:
-            return min(dist_list)/math.sqrt((VA['x'] - VB['x'])**2 + (VA['y'] - VB['y'])**2)
+            return min(dist_list) / math.sqrt((VA['x'] - VB['x']) ** 2 + (VA['y'] - VB['y']) ** 2)
         else:
             return -1
 
@@ -505,7 +518,7 @@ class MergingExtractionClass(object):
     def run(self):
         if self.savemode == 'test':
             self.create_output_folder(self.rootPath, 'output')
-            path = self.rootPath+"/output/"
+            path = self.rootPath + "/output/"
         else:
             self.create_output_folder(self.rootPath, 'result')
             path = self.savePath

src/figure/TTC_distribution.py
@@ -9,7 +9,7 @@ from loguru import logger
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import skew, kurtosis
+from scipy.stats import f, skew, kurtosis
 from TTC_acc_figure import create_output_folder
 
 
@@ -24,23 +24,46 @@ def main():
     create_output_folder(tesetPath, "distribution")
 
     # 初始化存储DataFrame的列表
-    dataColumns = ['Pearson1', 'Pearson2', 'Pearson3', 'Spearman1', 'Spearman2', 'Spearman3', 'DTW_Distance1',
-                   'DTW_Distance2', 'DTW_Distance3']
+    # dataColumns = ['Pearson1', 'Pearson2', 'Pearson3', 'Spearman1', 'Spearman2', 'Spearman3', 'DTW_Distance1',
+    #                'DTW_Distance2', 'DTW_Distance3']
+    dataColumns = ['DTW_Distance1', 'DTW_Distance2', 'DTW_Distance3']
 
     for col in dataColumns:
+
+        # # DTW 数据预处理 设置一个惩罚值
+        # if "DTW" in col:
+        #     dtw_temp = file[['DTW_Distance1', 'DTW_Distance2', 'DTW_Distance3']]
+        #     penalty_value = dtw_temp.apply(lambda row: row.apply(lambda x: row.max() if x == 0 else x), axis=1)
+        #     temp = penalty_value[col]
+        #     data = temp.where(temp != 999)
+        # else:
+        #     temp = file[col]
+        #     data = temp.where(temp != 999)
+
         temp = file[col]
-        data = temp.where(temp != 999)
+        data = temp.where((temp > 0) & (temp < 999))
+
         # 筛选掉空白值和无穷值
         data = data[~np.isnan(data)]
         data = data[np.isfinite(data)]
 
         # 画图设置
-        plt.figure(dpi=150)
+        plt.figure(dpi=1500)
         sns.set(style='whitegrid')
-        sns.set_style("whitegrid", {"axes.facecolor": "#e9f3ea"})
+        sns.set_style("whitegrid", {"axes.facecolor": "#FFFFFF"})
 
-        # 绘制分布图
-        sns.histplot(data, kde=True)
+        # # 绘制分布图
+        # sns.histplot(data, kde=True)
+
+        # 使用seaborn绘制直方图
+        plt.figure(figsize=(10, 6))
+        if '1' in col:
+            ax = sns.histplot(data, bins=500, kde=False, edgecolor=None, color='#5470C6')
+        else:
+            ax = sns.histplot(data, bins=1000, kde=False, edgecolor=None, color='#5470C6')
+
+        # 获取y轴的最大值
+        y_max = ax.get_ylim()[1]
 
         # 计算中位数、平均值、方差和峰度偏度
         median = np.nanmedian(data)
@@ -50,16 +73,33 @@ def main():
         kurt = kurtosis(data)
 
         # 标注中位数和平均值
-        plt.axvline(x=median, color='r', linestyle='--', label=f'Median: {median:.2f}')
-        plt.axvline(x=mean, color='g', linestyle='--', label=f'Mean: {mean:.2f}')
+        plt.axvline(x=median, color='#AAD795', linestyle='--', linewidth=4, label=f'Median: {median:.2f}')
+        plt.axvline(x=mean, color='#FAC858', linestyle='--', linewidth=4, label=f'Mean: {mean:.2f}')
 
-        # 显示方差和峰度偏度
-        plt.text(2, 200, f'Variance: {variance:.2f}', fontsize=10)
-        plt.text(2, 180, f'Skewness: {skewness:.2f}', fontsize=10)
-        plt.text(2, 160, f'Kurtosis: {kurt:.2f}', fontsize=10)
+        # 定义文本位置
+        y_position = y_max * 0.9  # 初始位置，设为y轴最大值的90%
+        y_step = y_max * 0.1  # 每行文本之间的距离，设为y轴最大值的5%
 
-        plt.legend()
+        # 显示方差、偏度和峰度
+        plt.text(15, y_position, f'Variance: {variance:.2f}', fontsize=20)
+        plt.text(15, y_position - y_step, f'Skewness: {skewness:.2f}', fontsize=20)
+        plt.text(15, y_position - 2 * y_step, f'Kurtosis: {kurt:.2f}', fontsize=20)
+
+        ax.set_title('Histogram with Mean and Median', fontsize=20)
+        ax.set_xlabel('Value', fontsize=20)
+        ax.set_ylabel('Frequency', fontsize=20)
+
+        plt.legend(fontsize=20)
+        # 调整刻度标签的字号
+        ax.tick_params(axis='both', which='major', labelsize=20)
+        # 设置x轴范围为0到50
+        if '1' in col:
+            ax.set_xlim(0, 100)
+        else:
+            ax.set_xlim(0, 50)
         plt.savefig(figurepath+f"{col}.png")
+
+    logger.info("Finish.")
 
 
 if __name__ == '__main__':

utils/common.py
@@ -210,7 +210,11 @@ def ellipses_tangent_time(a1, b1, theta1, x1, y1, vx, vy, a2, b2, theta2, x2, y2
 
     # 找到相切时刻
     t_solutions = fsolve(tangent_equation, guess_t, fprime=None, col_deriv=False, maxfev=1000)
-    t_solution = min((x for x in t_solutions if x > 0), default=None)
+    temp = min((x for x in t_solutions if x > 0), default=None)
+    if temp is None:
+        t_solution = t_solutions[0]
+    else:
+        t_solution = temp
 
     # 检查解是否有效
     if t_solution is not None:
@@ -219,7 +223,7 @@ def ellipses_tangent_time(a1, b1, theta1, x1, y1, vx, vy, a2, b2, theta2, x2, y2
         y_t = y2 + vy * t_solution
         return t_solution, x_t, y_t
     else:
-        return 999, None, None
+        return None, None, None
 
 
 def sigmoid_gradient(x):
@@ -277,7 +281,7 @@ def reload_ellipses_tangent_time(a1, b1, theta1, x1, y1, vx, vy, a2, b2, theta2,
         return np.abs(eq1_value) + np.abs(eq2_value)
 
     # 尝试不同的初始猜测值
-    t_values = np.linspace(0, 50, 100)  # 从0到10，取100个值作为初始猜测值
+    t_values = np.linspace(10, 50, 100)  # 从0到10，取100个值作为初始猜测值
     best_t = None
     best_value = np.inf
 